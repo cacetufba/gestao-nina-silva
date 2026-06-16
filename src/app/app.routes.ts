73f9060f37f1c0b144a080ae257ad9e902080e86
Andresa43 <andresa.ifba@gmail.com>
2026-06-15 22:15:55 -0300

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Membros } from './pages/membros/membros';
+import { MulheresCiencia } from './pages/mulheres-ciencia/mulheres-ciencia';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'membros', component: Membros },
+  { path: 'mulheres-ciencia', component: MulheresCiencia },
+
 ];
